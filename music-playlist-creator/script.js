ede80fb967eb2a4236a61f9fd4b191d93a76cd84
Kailer Laino <kailer@meta.com>
2025-06-04 12:02:04 -0700

    complete rough draft of milestones 2 and 3

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,21 +1,59 @@
 // JavaScript for Opening and Closing the Modal
-const modal = document.getElementById("playstModal");
+const modal = document.getElementById("playlistModal");
 const span = document.getElementsByClassName("close")[0];
 
 function openModal(festival) {
-   document.getElementById('festivalName').innerText = festival.name;
-   document.getElementById('festivalImage').src = festival.imageUrl;
-   document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
-   document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
-   document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
-   modal.style.display = "block";
+  //    document.getElementById('festivalName').innerText = festival.name;
+  //    document.getElementById('festivalImage').src = festival.imageUrl;
+  //    document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
+  //    document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
+  //    document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
+  modal.style.display = "block";
 }
 
-span.onclick = function() {
-   modal.style.display = "none";
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+fetch("data/data.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error fetching data");
+    }
+    return response.json(); // Assuming the server sends JSON
+  })
+  .then(data => {
+    renderPlaylists(data)
+  })
+  .catch(error => {
+    console.error('There was a problem rendering playlists:', error);
+  });
+
+function renderPlaylists(playlists) {
+  const container = document.getElementById("playlist-cards");
+
+
+  playlists.forEach(playlist => {
+    const playlistDiv = document.createElement("div");
+    playlistDiv.className = "playlist-item";
+
+    playlistDiv.innerHTML = `
+      <img src=${playlist.playlist_art}
+            alt="image of playlist"
+            height="100"
+            width="100">
+      <h3>${playlist.playlist_name}</h3>
+      <p>${playlist.playlist_author}</p>
+      <p>♡${playlist.playlist_likes}</p>
+    `
+    console.log(playlist.playlist_name);
+    container.appendChild(playlistDiv);
+  })
 }
-window.onclick = function(event) {
-   if (event.target == modal) {
-      modal.style.display = "none";
-   }
-}
+
